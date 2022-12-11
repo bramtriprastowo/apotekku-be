f59fd8f40837b6d7a4659698aaea30e4ccc0b52d
Bramono Triprastowo <bramonotriprastowo@yahoo.com>
2022-12-11 20:44:25 +0700

    add api docs

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const mainRouter = require("./routes/index")
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+const mainRouter = require("./routes/index");
 
 app.use(express.json());
 app.use(cors());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use("/api/v1", mainRouter)
 
